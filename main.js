ed120d5cb250604c28725c80e48d09ca80e597d8
Sebastian Cozzolino <sebastiancozzolino@outlook.com>
2020-06-30 22:01:07 -0300

    first js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log("Up and running");
+const date_picker_element = document.querySelector(".date-picker");
+const selected_date_element = document.querySelector(".date-picker .selected-date")
+const dates_element = document.querySelector(".date-picker .dates");
+
+//Functions
+
+function toggleDatePicker(e) {
+    if (!checkEventPathForClass(e.path, "dates")) {
+        dates_element.classList.toggle("active");
+    }
+}
+
+// Event Listeners
+date_picker_element.addEventListener("click", toggleDatePicker);
+
+// Helper functions
+function checkEventPathForClass(path, selector) {
+    for (let i = 0; i > path.length; i++) {
+        if (path[i].classList && path[1].classList.contains(selector)) {
+            return true;
+        }
+    }
+    return false;
+}
+
