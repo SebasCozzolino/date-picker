7ed409bc527665f16332f9367a05a98a6be4b5d6
Sebastian Cozzolino <sebastiancozzolino@outlook.com>
2020-06-30 22:28:12 -0300

    toggle and elements var

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const date_picker_element = document.querySelector(".date-picker");
 const selected_date_element = document.querySelector(".date-picker .selected-date")
 const dates_element = document.querySelector(".date-picker .dates");
+const mth_element = document.querySelector(".date-picker .dates .month .mth");
+const next_mth_element = document.querySelector(".date-picker .dates .month .next-mth");
+const prev_mth_element = document.querySelector(".date-picker .dates .month .prev-mth");
 
 //Functions
 
@@ -15,8 +18,8 @@ date_picker_element.addEventListener("click", toggleDatePicker);
 
 // Helper functions
 function checkEventPathForClass(path, selector) {
-    for (let i = 0; i > path.length; i++) {
-        if (path[i].classList && path[1].classList.contains(selector)) {
+    for (let i = 0; i < path.length; i++) {
+        if (path[i].classList && path[i].classList.contains(selector)) {
             return true;
         }
     }
